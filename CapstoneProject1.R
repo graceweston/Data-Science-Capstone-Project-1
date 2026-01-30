ba957b182dc6653aff46f2732059a04985afd775
mlagner <macklagner@gmail.com>
2026-01-30 14:04:52 -0500

    Update CapstoneProject1.R
    cleaned Games data

--- CapstoneProject1.R
+++ CapstoneProject1.R
@@ -11,3 +11,30 @@ stadiums <- stadiums |>
       tz_lookup_coords(Lat, Long, method = "accurate") == "America/Los_Angeles" ~ "PST"))
   
 stadiums
+
+games <- read.csv(file.choose())
+
+library(dplyr)
+library(tidyverse)
+
+# combine column to rename
+games <- games %>%
+  mutate(HomeTeam = paste(hometeamCity, hometeamName, sep = " "))
+
+games <- games %>%
+  mutate(AwayTeam = paste(awayteamCity, awayteamName, sep = " "))
+
+
+games <- games %>%
+  rename(winner_num = winner) %>%
+  mutate(
+    winningTeam = ifelse(winner_num == 1, "Home", "Away"),
+    winner = ifelse(winningTeam == "Home", HomeTeam, AwayTeam)
+  ) %>%
+  select(-winner_num)
+
+#delete unneeded columns
+games <- games %>%
+  select(-gameId, -hometeamId, awayteamId, -gameType, -arenaId, -gameLabel, -gameSubLabel, -seriesGameNumber)
+
+
