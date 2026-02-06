41c24528b2f835ee2c97a55ba93db4042b2ac0a8
carterwilliams26 <williamscarter230@gmail.com>
2026-02-06 13:42:45 -0500

    Update CapstoneProject1.R

--- CapstoneProject1.R
+++ CapstoneProject1.R
@@ -132,7 +132,9 @@ HomeCourt <- games_updated |>
 HomeCourt <- HomeCourt |>
   select(GameId, Game_Date, Game_Time, HomeTeam, HomeTeamLat, HomeTeamLong, HomeTeamTimeZone, AwayTeam, AwayTeamLat, AwayTeamLong, AwayTeamTimeZone, HomeScore, AwayScore, WinningTeam, Winner, Home_B2B, Away_B2B, Attendance, League.y, Division.y)
 
-View(HomeCourt)
+HomeCourt <- HomeCourt |>
+  filter(WinningTeam != "NA")
+
 
 
 
